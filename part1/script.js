ed5dac2d0d312abdeb46cf4025878fbcad3fdfec
ViKono <vic.konopelko@gmail.com>
2020-03-18 23:36:34 +0200

    Added validation and one item for obj appData

--- part1/script.js
+++ part1/script.js
@@ -1,8 +1,6 @@
 let money = +prompt("What is your monthly budget?", ''),
     time = +prompt("Enter a date in the format YYYY-MM-DD", '');
 
-let moneyForOneDay = money / 30; 
-
 let appData = {
     budget: money,
     timeData: time,
@@ -16,7 +14,14 @@ for (let i = 0; i < 2; i++) {
     let enterCostItem = prompt("Enter a mandatory cost item this month", ''),
         howMach = +prompt("How much will it cost?", '');
     
-    appData.expenses[enterCostItem] = howMach;
+    if ( (typeof(enterCostItem)) === 'string' && (typeof(enterCostItem)) != null && (typeof(howMach)) != null && enterCostItem != '' && howMach != '' && enterCostItem.length < 50) {
+        console.log("Validation was successful!")
+        appData.expenses[enterCostItem] = howMach;
+    } else {
+        i = 0;
+    }
 }
+
+appData.moneyPerDay = appData / 30;
 
 alert("1 day budget = " + moneyForOneDay);
