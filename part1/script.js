8416725df5797821963187b54ca8e9c451f3b2d3
ViKono <vic.konopelko@gmail.com>
2020-03-18 23:26:11 +0200

    added first loop

--- part1/script.js
+++ part1/script.js
@@ -1,10 +1,5 @@
 let money = +prompt("What is your monthly budget?", ''),
-    time = prompt("Enter a date in the format YYYY-MM-DD", '');
-
-let enterCostItem = prompt("Enter a mandatory cost item this month", ''),
-    howMach = prompt("How much will it cost?", ''),
-    enterCostItem2 = prompt("Enter a mandatory cost item this month", ''),
-    howMach2 = prompt("How much will it cost?", '');
+    time = +prompt("Enter a date in the format YYYY-MM-DD", '');
 
 let moneyForOneDay = money / 30; 
 
@@ -17,7 +12,11 @@ let appData = {
     savings: false
 }
 
-appData.expenses[enterCostItem] = howMach;
-appData.expenses[enterCostItem2] = howMach2;
+for (let i = 0; i < 2; i++) {
+    let enterCostItem = prompt("Enter a mandatory cost item this month", ''),
+        howMach = +prompt("How much will it cost?", '');
+    
+    appData.expenses[enterCostItem] = howMach;
+}
 
 alert("1 day budget = " + moneyForOneDay);
