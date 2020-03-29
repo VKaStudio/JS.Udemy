0d004684206b42715d9799acd654e1f370d02487
ViKono <vic.konopelko@gmail.com>
2020-03-30 01:11:38 +0300

    Added modal window + fix time

--- part2 LandingPage/js/script.js
+++ part2 LandingPage/js/script.js
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
     // Timer
 
-    let deadline = '2020-03-28';
+    let deadline = '2020-03-31';
 
     function getTimeRemaining(endtime){
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -81,4 +81,35 @@ window.addEventListener('DOMContentLoaded', function(){
         }
     }
     setClock('timer', deadline);
+    
+    // modal 
+    
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function(){
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    // add modal for every btn in tabs
+
+    let btnLearnMore = document.querySelectorAll('.description-btn');
+
+    for (let i = 0; i < btnLearnMore.length; i++) {
+        let element = btnLearnMore[i];
+        element.addEventListener('click', function() {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        }); 
+    }
 });
